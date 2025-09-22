41c140b853184fb2a04be51cfa8c744a6080bb89
O-Raynold <vigourhybrid@gmail.com>
2025-09-22 15:58:17 +0300

    Add files via upload

---
+++ Age , height  and bank balance.c
@@ -0,0 +1,29 @@
+// Age , height and bank balance
+/*  first semester 
+Name; Raynold 
+Reg; PA106/G/28820/25
+*/
+#include<stdio.h>
+int main() {
+ //enter variables
+  int height ;
+  int ID;
+  int balance;
+  
+  printf("Enter your height:");
+  scanf("%d" ,& height);
+  printf("\nyour height is=%d" , height);
+  
+  printf("\n Enter your ID:");
+  scanf("%d" ,&ID);
+  printf("\n ID =%d" ,ID);
+  
+  printf("\nEnter your balance:");
+  scanf("%d" , &balance);
+  printf("\nbalance=%d" ,balance);
+
+
+    return 0;
+}
+
+    
