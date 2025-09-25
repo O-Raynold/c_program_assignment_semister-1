bed3ce285f2605274fd57bd8b8fedc51c8c60921
O-Raynold <vigourhybrid@gmail.com>
2025-09-25 12:00:39 +0300

    Add files via upload

---
+++ Raynold s units this semester .c
@@ -0,0 +1,23 @@
+// My basic common units 
+/*  first semester 
+no of units
+*/
+#include<stdio.h>
+int main(){
+printf("My course units this semester\n" );
+int  units = 7;//number of units
+printf("number of units =%d\n" ,units);
+
+printf("Descrete Mathematics\n");
+printf("Math of science\n");
+printf("Computer system and organisation\n");
+printf("Communication skills\n");
+printf("Fundamental of ICT\n");
+printf("System analysis and design\n");
+printf("Introduction to programming and problem solving");
+
+
+return 0;
+}
+
+    
